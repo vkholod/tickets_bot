ebc63858cf7dfdfc260f8b745a5426b02360ad33
Vova Kholodenko <kholodenko.vova@gmail.com>
2019-12-05 22:45:48 +0200

    Restored checker

--- src/main/java/com/vkholod/wizzair/tickets_bot/job/TimetableCheckJob.java
+++ src/main/java/com/vkholod/wizzair/tickets_bot/job/TimetableCheckJob.java
@@ -29,17 +29,22 @@ public class TimetableCheckJob implements Job {
         RedisStorage storage = RedisStorage.class.cast(data.get("storage"));
 
         try {
-//            Timetable fetchedTimetable = timetableService.getTimetable(timetableRequestDto);
-//            Optional<Timetable> savedTimetable = storage.findTimetableInRedis(timetableRequestDto.generateRedisKey());
-//
-//            if (!savedTimetable.isPresent()) {
-//                storage.saveTimetableInRedis(timetableRequestDto.generateRedisKey(), fetchedTimetable);
-//            } else if (!savedTimetable.get().equals(fetchedTimetable)) {
-//                storage.saveTimetableInRedis(timetableRequestDto.generateRedisKey(), fetchedTimetable);
-//                bot.sendMessage(RoundTripsUtil.buildMessage(fetchedTimetable));
-//            } else {
+            System.out.println("TimetableCheckJob start");
+            Timetable fetchedTimetable = timetableService.getTimetable(timetableRequestDto);
+            Optional<Timetable> savedTimetable = storage.findTimetableInRedis(timetableRequestDto.generateRedisKey());
+
+            if (!savedTimetable.isPresent()) {
+                storage.saveTimetableInRedis(timetableRequestDto.generateRedisKey(), fetchedTimetable);
+                System.out.println("TimetableCheckJob saved timetable to Redis");
+            } else if (!savedTimetable.get().equals(fetchedTimetable)) {
+                System.out.println("TimetableCheckJob found diff");
+                storage.saveTimetableInRedis(timetableRequestDto.generateRedisKey(), fetchedTimetable);
+                bot.sendMessage(RoundTripsUtil.buildMessage(fetchedTimetable));
+            } else {
+                System.out.println("TimetableCheckJob no changes");
                 bot.sendSilentMessage("I am alive, no changes yet");
-//            }
+            }
+            System.out.println("TimetableCheckJob finish");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
